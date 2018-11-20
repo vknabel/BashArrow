887a077780bbf2ad61d35536e103b52d411be4c2
Valentin Knabel <dev@vknabel.com>
2018-11-20 16:45:01 +0100

    Added missing SwiftPM product

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.2
+
+* Missing SwiftPM product
+
 ## 0.2.1
 
 * Rely on [jakeheis/SwiftCLI](https://github.com/jakeheis/SwiftCLI) in order to pass interrupts correctly
--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "BashArrow",
+    products: [
+        .executable(name: "BashArrow", targets: ["BashArrow"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vknabel/ArrowKit.git", from: "0.2.1"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.1.2"),
